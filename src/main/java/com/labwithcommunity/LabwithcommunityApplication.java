29e0971b062905a60782fd0f5a9fc5c9ed45d4e6
Dominik Kulpowicz <dominik.kulpowicz@gmail.com>
2024-09-18 20:57:31 +0200

    initial_commit

---
+++ src/main/java/com/labwithcommunity/LabwithcommunityApplication.java
@@ -0,0 +1,13 @@
+package com.labwithcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabwithcommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabwithcommunityApplication.class, args);
+    }
+
+}
