e8add5c0f7fa329ce9f7e69cd3477203fd094a4d
Dominik Kulpowicz <dominik.kulpowicz@gmail.com>
2024-09-19 18:54:06 +0200

    adding user Facade

--- src/main/java/com/labwithcommunity/domain/user/UserFacade.java
+++ src/main/java/com/labwithcommunity/domain/user/UserFacade.java
@@ -1,9 +1,25 @@
 package com.labwithcommunity.domain.user;
 
+import com.labwithcommunity.domain.user.dto.UserCreateDto;
+import com.labwithcommunity.domain.user.dto.UserCreateResponseDto;
+import com.labwithcommunity.domain.user.dto.UserResponseDto;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Set;
 
 @RequiredArgsConstructor
 public class UserFacade {
 
     private final UserService userService;
+
+    public UserResponseDto findUserByNickname(String nickname) {
+        return userService.getUserByNickname(nickname);
+    }
+
+    public UserCreateResponseDto registerUser(UserCreateDto userCreateDto) {
+        return userService.register(userCreateDto);
+    }
+    private boolean addRolesToUser(Set<UserRoles> userRoles, String nickname){
+        return userService.addRoleToUser(userRoles, nickname);
+    }
 }
