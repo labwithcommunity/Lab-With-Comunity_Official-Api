fc8d9457a99115cb5f55322f32f2a90e31168b02
Łukasz Kośmiński <polde@protonmail.com>
2024-09-22 19:16:28 +0200

    add validatino user, add GlobalExceptionHandler

--- src/main/java/com/labwithcommunity/domain/user/dto/UserCreateDto.java
+++ src/main/java/com/labwithcommunity/domain/user/dto/UserCreateDto.java
@@ -1,18 +1,27 @@
 package com.labwithcommunity.domain.user.dto;
 
 import com.labwithcommunity.domain.user.Technologies;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.Email;
 
 import java.util.Set;
 
 @Builder
 public record UserCreateDto(
+
+        @Size(min = 4, message = "User name must be at least 4 characters long")
         String username,
+
+        @Size(min = 4, message = "Password must be at least 4 characters long")
         String password,
+
+        @Email(message = "Invalid email format")
         String email,
-        Set<Technologies>technologies
+
+        @NotNull(message = "Technologies must not be null")
+        Set<Technologies> technologies
 ) {
 }
+
