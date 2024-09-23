3349a602674a3dbf9fc133e107df117fc74a5a63
Dominik Kulpowicz <dominik.kulpowicz@gmail.com>
2024-09-24 01:22:57 +0200

    Typo in AuthController

--- src/main/java/com/labwithcommunity/infrastructure/user/controller/AuthController.java
+++ src/main/java/com/labwithcommunity/infrastructure/user/controller/AuthController.java
@@ -6,14 +6,14 @@ import com.labwithcommunity.domain.user.dto.UserCreateResponseDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 @RestController
 @RequestMapping("/api/v1/public")
 @RequiredArgsConstructor
 @CrossOrigin("*")
-public class AuthControler {
+class AuthController {
 
     private final UserFacade userFacade;
 
--- src/main/java/com/labwithcommunity/infrastructure/validation/GlobalExceptionHandler.java
+++ src/main/java/com/labwithcommunity/infrastructure/validation/GlobalExceptionHandler.java
@@ -10,8 +10,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.HashMap;
 import java.util.Map;
+
 @ControllerAdvice
-public class GlobalExceptionHandler {
+class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
