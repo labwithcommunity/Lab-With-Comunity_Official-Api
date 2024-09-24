5408f550bbda3329259450ed3622e0675debdf2a
Dominik Kulpowicz <dominik.kulpowicz@gmail.com>
2024-09-24 18:40:55 +0200

    optimie imports

--- src/main/java/com/labwithcommunity/infrastructure/user/controller/UserController.java
+++ src/main/java/com/labwithcommunity/infrastructure/user/controller/UserController.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
--- src/test/java/com/labwithcommunity/domain/user/UserFacadeTest.java
+++ src/test/java/com/labwithcommunity/domain/user/UserFacadeTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
