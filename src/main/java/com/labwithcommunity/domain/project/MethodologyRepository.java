7ffc0f6fd69234f1a91620132dbb522f59b5132c
Dominik Kulpowicz <dominik.kulpowicz@gmail.com>
2024-10-13 19:01:00 +0200

    adding methodology service implementation

--- src/main/java/com/labwithcommunity/domain/project/MethodologyRepository.java
+++ src/main/java/com/labwithcommunity/domain/project/MethodologyRepository.java
@@ -3,4 +3,5 @@ package com.labwithcommunity.domain.project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 interface MethodologyRepository extends JpaRepository<MethodologyEntity,Integer> {
+    boolean existsByMethodologyName(String name);
 }
--- src/main/java/com/labwithcommunity/domain/project/MethodologyService.java
+++ src/main/java/com/labwithcommunity/domain/project/MethodologyService.java
@@ -1,6 +1,8 @@
 package com.labwithcommunity.domain.project;
 
 import com.labwithcommunity.domain.project.dto.methodology.MethodologyCreateDto;
+import com.labwithcommunity.domain.project.exception.methodology.MethodologyExceptionMessages;
+import com.labwithcommunity.domain.project.exception.methodology.MethodologyNotFoundException;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -9,17 +11,17 @@ class MethodologyService {
     private final MethodologyRepository methodologyRepository;
 
     MethodologyEntity getMethodologyById(int id) {
-
-        //todo exception handling
-
-        return methodologyRepository.findById(id).orElse(null);
+        return methodologyRepository.findById(id)
+                .orElseThrow(()->new MethodologyNotFoundException(
+                        MethodologyExceptionMessages.METHODOLOGY_NOT_FOUND.getMessage()));
     }
 
     MethodologyCreateDto createMethodology(MethodologyCreateDto methodologyCreateDto) {
-
-        methodologyRepository.save(new MethodologyEntity(
-                methodologyCreateDto.methodologyName()
-        ));
+        if (!methodologyRepository.existsByMethodologyName(methodologyCreateDto.methodologyName())){
+            methodologyRepository.save(new MethodologyEntity(
+                    methodologyCreateDto.methodologyName()
+            ));
+        }
         return methodologyCreateDto;
     }
 }
